5df0f57735fd44bdce550a035d9c912bc555042e
Renan Pinheiro <renan.pinheiro@accenture.com>
2022-11-24 22:30:18 -0300

    setup initial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Admin from "./pages/Admin";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "/admin", element: <Admin /> },
+]);
+
+export { router };
